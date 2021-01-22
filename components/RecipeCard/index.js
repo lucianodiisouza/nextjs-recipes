2489d05c626781b1fc4d19021b7f8a051ef68a86
lucianodiisouza <lucianodiisouza@hotmail.com>
2021-01-22 00:10:50 -0300

    done: display recipes component

--- components/RecipeCard/index.js
+++ components/RecipeCard/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+import RecipeCardStyled from "./styles";
+
 export default function RecipeCard(props) {
   return (
-    <div>
-      <h5>Receita</h5>
-    </div>
+    <figure className="recipe-card">
+      <style jsx>{RecipeCardStyled}</style>
+      <Link href={props.link}>
+        <a>
+          <img
+            src={props.picture}
+            alt={props.name}
+            className="recipe-picture"
+          />
+        </a>
+      </Link>
+      <div className="recipe-category">{props.category}</div>
+      <figcaption className="recipe-name">{props.name}</figcaption>
+    </figure>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
-import { Header, Footer } from "../components";
+import { useState, useEffect } from "react";
+import { Header, Footer, RecipeCard } from "../components";
+import { RecipeService } from "../services/RecipeService";
 
 export default function Home() {
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    RecipeService.listAll().then(setList);
+  }, []);
+
   return (
     <div>
       <Header title="NextRecipes" />
+      {list.map((recipe) => (
+        <RecipeCard
+          key={recipe.id}
+          link={"/"}
+          category={recipe.category}
+          name={recipe.name}
+          picture={recipe.img}
+        />
+      ))}
+
       <Footer />
     </div>
   );

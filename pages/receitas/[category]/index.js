c2a3a048fbe6d4466b1a3de6ebd2127ae87b5fad
lucianodiisouza <lucianodiisouza@hotmail.com>
2021-01-22 10:52:35 -0300

    done: categories dynamic page

---
+++ pages/receitas/[category]/index.js
@@ -0,0 +1,50 @@
+import slugify from "slugify";
+
+import { Header, Footer, RecipeCategory } from "../../../components";
+import { RecipeService } from "../../../services/RecipeService";
+
+export async function getStaticPaths() {
+  let categories = {};
+  const recipes = await RecipeService.listAll();
+
+  recipes.forEach((recipe) => {
+    categories[recipe.category] = true;
+  });
+
+  const paths = Object.keys(categories).map((category) => {
+    return {
+      params: {
+        category: slugify(category).toLowerCase(),
+      },
+    };
+  });
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context) {
+  const { category } = context.params;
+  const recipes = (await RecipeService.listAll()).filter(
+    (recipe) => slugify(recipe.category).toLowerCase() === category
+  );
+
+  return {
+    props: {
+      recipes,
+    },
+  };
+}
+
+export default function RecipeCategoryPage({ recipes = [] }) {
+  if (recipes.length === 0) {
+    return <div>Nenhuma receita</div>;
+  }
+
+  return (
+    <div>
+      <Header title={`NextRecipes - ${recipes[0].category}`} />
+      <RecipeCategory category={recipes[0].category} recipeList={recipes} />
+      <Footer />
+    </div>
+  );
+}
